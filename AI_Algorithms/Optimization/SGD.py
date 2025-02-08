185e0fc34d98cbd91ea6b89ac4d1f9b2b7f696f3
wayne.yu <wayne.yu@jtexpress.com>
2025-02-08 18:00:04 +0800

    Update SGD (unfinished)

--- AI_Algorithms/Optimization/SGD.py
+++ AI_Algorithms/Optimization/SGD.py
@@ -1,2 +1,13 @@
+#%%
 import torch
-X = torch.rand(1)
+torch.manual_seed(42)
+# 模拟数据
+X = torch.rand(2,100, dtype=torch.float32)*40 - 20
+src_params = torch.tensor([7.6, 8.457], dtype=torch.float32)
+Y = torch.matmul(src_params,X)
+Y += torch.randn(Y.shape) * 1.5
+# %%
+W = torch.rand(2, dtype=torch.float32)
+b = torch.zeros(1)
+def function(X):
+    return torch.matmul(W, X) + b
